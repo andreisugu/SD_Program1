172740f87ac2ce698eb78a30be08c442aa671268
Andrei Sugubete <andreisugu@gmail.com>
2023-04-01 19:01:46 +0300

    Doubly Linked Lists Definitions

--- main.c
+++ main.c
@@ -1,16 +1,136 @@
 #include "vma.h"
-#define DIE(assertion, call_description)				\
-	do {								\
-		if (assertion) {					\
-			fprintf(stderr, "(%s, %d): ",			\
-					__FILE__, __LINE__);		\
-			perror(call_description);			\
-			exit(errno);				        \
-		}							\
-	} while (0)
 
 #define MAX_STR 256
 
+// Doubly-linked lists definitions
+dll_list_t*
+dll_create(unsigned int data_size)
+{
+	// Allocating memory for structure and set variables
+    dll_list_t *lista = calloc(1, sizeof(dll_list_t));
+    DIE(lista == NULL, "calloc() for list failed");
+    lista->head = NULL;
+    lista->data_size = data_size;
+    lista->size = 0;
+    return lista;
+}
+
+dll_node_t*
+dll_get_nth_node(dll_list_t* list, unsigned int n)
+{
+	// If list is empty, return null
+    if(list->size == 0)
+        return NULL;
+	// We adjust n so it cycles
+	n = (n + 1) % list->size - 1;
+    if(n == -1)
+		n = list->size - 1;
+    // We go to the n-th node
+    dll_node_t *nod = list->head;
+    while(n > 0) {
+        nod = nod->next;
+        n--;
+    }
+    return nod;
+}
+
+void
+dll_add_nth_node(dll_list_t* list, unsigned int n, const void* new_data)
+{
+	// n can't go over the list size
+	if(n > list->size)
+			n = list->size;
+	// Allocating the node's and it's data's memory
+    dll_node_t *nod = calloc(1, sizeof(dll_node_t));
+	DIE(nod == NULL, "calloc() for node failed");
+	nod->data = calloc(1, list->data_size);
+	DIE(nod->data == NULL, "calloc() for node data failed");
+    // Copying data into node
+	memcpy(nod->data, new_data, list->data_size);
+	// We find where we're supposed to place the new node and we make the connections
+	if(list->size == 0) {
+		// If this is the first node, we treat it specially so it's doubly linked
+        nod->prev = nod;
+        nod->next = nod;
+        list->head = nod;
+    } else {
+		if(n == 0) {
+			nod->next = list->head;
+			nod->prev = list->head->prev;
+			list->head = nod;
+			nod->next->prev = nod;
+			nod->prev->next = nod;
+		} else if(n == list->size) {
+			nod->next = list->head;
+			nod->prev = dll_get_nth_node(list, list->size - 1);
+			nod->prev->next = nod;
+			nod->next->prev = nod;
+		} else {
+			// Usual case
+			nod->prev = dll_get_nth_node(list, n - 1);
+			nod->next = nod->prev->next;
+      		nod->prev->next = nod;
+			nod->next->prev = nod;
+		}
+    }
+    // We increment the size
+    list->size++;
+}
+
+dll_node_t*
+dll_remove_nth_node(dll_list_t* list, unsigned int n)
+{
+	dll_node_t *nod;
+	if(list->size > 0) {
+		// We get the specified node
+		if(n + 1 > list->size)
+			n = list->size - 1;
+		nod = dll_get_nth_node(list, n);
+		// We adjust the surrounding nodes' neighbors
+		if(n == 0)
+			list->head = list->head->next;
+		nod->prev->next = nod->next;
+		nod->next->prev = nod->prev;
+		list->size--;
+	} else {
+		// In case of an error, we make a dummy node
+		nod = calloc(1, sizeof(dll_node_t));
+		DIE(nod == NULL, "calloc() for dummy node failed");
+		nod->data = calloc(1, list->data_size);
+		DIE(nod->data == NULL, "calloc() for dummy node data failed");
+	}
+	return nod;
+}
+
+unsigned int
+dll_get_size(dll_list_t* list)
+{
+    return list->size;
+}
+
+
+// Arenas and blocks definitions
+arena_t *alloc_arena(const uint64_t size) {
+    arena_t *ar;
+    // Alocam adresa pentru arena
+    ar = calloc(1, sizeof(arena_t));
+    DIE(!ar, "calloc() for arena failed");
+    // Alocam adresa pentru lista de blocuri si setam variabilele
+    ar->arena_size = size;
+    ar->alloc_list = calloc(1, sizeof(dll_list_t));
+    DIE(!(ar->alloc_list), "calloc() for arena alloc_list failed");
+    // Intoarcem adresa arenei
+    return ar;
+}
+
+void dealloc_arena(arena_t *arena) {
+    // Dealocam lista de blocuri
+    free(arena->alloc_list);
+    // Dealocam arena
+    free(arena);
+}
+
+void alloc_block(arena_t *arena, const uint64_t address, const uint64_t size);
 
 int main(void)
 {
--- vma.h
+++ vma.h
@@ -1,10 +1,50 @@
 #pragma once
 #include <inttypes.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 
-/* TODO : add your implementation for doubly-linked list */
-typedef struct { } list_t;
+#define DIE(assertion, call_description)				\
+	do {								\
+		if (assertion) {					\
+			fprintf(stderr, "(%s, %d): ",			\
+					__FILE__, __LINE__);		\
+			perror(call_description);			\
+			exit(errno);				        \
+		}							\
+	} while (0)
 
+// Doubly-linked list definitions
+typedef struct dll_node_t dll_node_t;
+struct dll_node_t
+{
+    void* data;
+    dll_node_t *prev, *next;
+};
+
+typedef struct {
+	dll_node_t* head;
+    unsigned int data_size;
+    unsigned int size;
+ } dll_list_t;
+
+dll_list_t*
+dll_create(unsigned int data_size);
+
+dll_node_t*
+dll_get_nth_node(dll_list_t* list, unsigned int n);
+
+void
+dll_add_nth_node(dll_list_t* list, unsigned int n, const void* new_data);
+
+dll_node_t*
+dll_remove_nth_node(doubly_linked_list_t* list, unsigned int n);
+
+unsigned int
+dll_get_size(dll_list_t* list);
+
+// Arenas and Blocks definitions
 typedef struct {
 	uint64_t start_address;
 	size_t size;
